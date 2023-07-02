927bcf0d94d09bea26977d5db0b50b442f070b31
Isaac Lima de Sousa <isaacsousa@mobitbrasil.com.br>
2023-07-02 17:42:05 -0300

    Fixing push mistake

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
-#include "FreeRTOS.h"
+#define StateGain 0.2
+#define SetPoint 430
+
 #include "pwmControl.h"
+#include "adControl.h"
 
 int main(void)
 {
-    pwmControl PWMm1 = pwmControl(TIM_OC2, TIM2_CNT, TIM2, RCC_TIM2,
-    GPIO1, GPIOA, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, 
-    GPIO_MODE_OUTPUT_50_MHZ, RCC_GPIOA);
+    uint16_t sensor1; 
+    int16_t error;
+    pwmControl PWMtimer_4(TIM4_CNT, TIM4, RCC_TIM4);
+    pwmControl PWMtimer_3(TIM3_CNT, TIM3, RCC_TIM3);
 
-    PWMm1.pwmStart(50);
+    PWMtimer_4.gpioSetup(TIM_OC1, GPIO6, GPIOB, RCC_GPIOB);
+    PWMtimer_4.gpioSetup(TIM_OC2, GPIO7, GPIOB, RCC_GPIOB);
+    PWMtimer_4.gpioSetup(TIM_OC3, GPIO8, GPIOB, RCC_GPIOB);
+    PWMtimer_4.gpioSetup(TIM_OC4, GPIO9, GPIOB, RCC_GPIOB);
+
+    PWMtimer_4.pwmWrite(100, TIM_OC1);
+    PWMtimer_4.pwmWrite(5, TIM_OC2);
+    PWMtimer_4.pwmWrite(35, TIM_OC3);
+    PWMtimer_4.pwmWrite(75, TIM_OC4);
+
+    PWMtimer_3.gpioSetup(TIM_OC4, GPIO1, GPIOB, RCC_GPIOB);
+    PWMtimer_3.pwmWrite(100, TIM_OC4);
 }
