4ae05615e4aa31bb48dfba19cbb1576cd7432db8
isaacsousa@mobitbrasil.com.br <isaacsousa@mobitbrasil.com.br>
2023-07-28 18:02:31 -0300

    fix merge erros

--- stm32_cm3/lib/inc/utils.h
+++ stm32_cm3/lib/inc/utils.h
@@ -1,23 +1,23 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define BUFFER_SIZE     100
+
 #include <stdint.h>
 
 typedef struct sensors_circular_buffer 
 {
-    uint8_t* begin;
-    uint8_t* end;
-    uint8_t* head;
-    uint8_t* tail;
-    uint32_t size;
+    float buffer[BUFFER_SIZE];
+    uint8_t head;
+    uint8_t tail;
+    bool full;
 } sensors_circular_buffer_t;
 
-sensors_circular_buffer_t* createCicularBuffer(uint32_t size);
-void destroyCircularBuffer(sensors_circular_buffer_t** buffer);
-uint32_t addToRingBuffer(sensors_circular_buffer_t* buffer, uint8_t value);
-uint8_t peekFromRingBuffer(sensors_circular_buffer_t* buffer);
-void nPeekFromRingBuffer(sensors_circular_buffer_t* buffer, uint8_t* data, uint32_t n);
-uint8_t removeFromRingBuffer(sensors_circular_buffer_t* buffer);
-uint32_t nRemoveFromRingBuffer(sensors_circular_buffer_t* buffer, uint8_t* data, uint32_t n);
+void cicularBufferInit(sensors_circular_buffer_t *circular_buffer);
+bool circularBufferEmpty(sensors_circular_buffer_t *circular_buffer);
+bool circularBufferFull(sensors_circular_buffer_t *circular_buffer);
+void circularBufferPush(sensors_circular_buffer_t *circular_buffer, float data);
+void circularBufferPop(sensors_circular_buffer_t *circular_buffer);
+
 
 #endif
